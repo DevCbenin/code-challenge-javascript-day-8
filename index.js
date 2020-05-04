a38cf4d7c53380652bf225ca7334f71d4ccbed76
devCbenin <devCbenin@gmail.com>
2020-05-04 13:47:24 +0100

    setup boiler plate and test

---
+++ index.js
@@ -0,0 +1,22 @@
+function luhn_check(value) {
+    // Accept only digits, dashes or spaces
+      if (/[^0-9-\s]+/.test(value)) return false;
+  
+      // The Luhn Algorithm. It's so pretty.
+      let nCheck = 0, bEven = false;
+      value = value.replace(/\D/g, "");
+  
+      for (var n = value.length - 1; n >= 0; n--) {
+          var cDigit = value.charAt(n),
+                nDigit = parseInt(cDigit, 10);
+  
+          if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+  
+          nCheck += nDigit;
+          bEven = !bEven;
+      }
+  
+      return (nCheck % 10) == 0;
+  }
+
+  module.exports = luhn_check;
